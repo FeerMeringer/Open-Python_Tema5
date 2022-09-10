d5f9dddb1a0e926a1f83bb5c34c2aadbdf82d52a
ludovico2299 <ferneringer@gmail.com>
2022-09-10 16:40:24 -0300

    Add exercises

---
+++ Bisiesto.py
@@ -0,0 +1,7 @@
+def anyoBisiesto():
+    anyo = int(input('¿Introduce los numero del año?: '))
+    if(anyo % 4 == 0 and (anyo % 100 != 0 or anyo % 400 == 0)):
+        print('Es un año bisiesto')
+    else:
+        print('NO es año bisiesto')
+print(anyoBisiesto())
